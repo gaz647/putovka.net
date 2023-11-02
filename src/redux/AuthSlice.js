880bded0b30c59988adf949c5c4229e56d716681
gaz647 <95932103+gaz647@users.noreply.github.com>
2023-11-02 17:24:31 +0100

    Přidán log do loginRedux

--- src/redux/AuthSlice.js
+++ src/redux/AuthSlice.js
@@ -109,6 +109,7 @@ export const loginRedux = createAsyncThunk(
   async (loginCredentials) => {
     try {
       console.log("loginRedux TRY část signInWithEmailAndPassword SPUŠTĚNA");
+      console.log(loginCredentials);
       await signInWithEmailAndPassword(
         auth,
         loginCredentials.loginEmail,
